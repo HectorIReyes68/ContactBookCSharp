58c26d57eb6305431489dadcfbd278e8c3712715
HectorIReyes68 <H3ctorr3yes899@gmail.com>
2026-05-01 21:10:42 -0400

    Add contact Class

--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ContactBook;
+
+public class Program
+{
+    public static void Main()
+    {
+        Contact c1 = new Contact();
+        Contact c2 = new Contact("John");
+        Contact c3 = new Contact("John", "Smith");
+        Contact c4 = new Contact("John", "Smith", "123-456-7890");
+        Contact c5 = new Contact("John", "Smith", "123-456-7890", "john.smith@example.com");
+        Contact c6 = new Contact(lname: "Smith", email: "john.smith@example.com");
+    }
+}
